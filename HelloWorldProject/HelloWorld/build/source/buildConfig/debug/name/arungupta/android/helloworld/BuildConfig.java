a85f0861204d7191312c3b77c91a5bf128aadef1
Arun Gupta <arun.p.gupta@oracle.com>
2013-09-06 17:41:57 -0700

    Adding a trivial and first working version of the application

---
+++ HelloWorldProject/HelloWorld/build/source/buildConfig/debug/name/arungupta/android/helloworld/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package name.arungupta.android.helloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
